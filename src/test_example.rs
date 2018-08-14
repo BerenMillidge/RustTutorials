0545514245dbcfbf5d7b3ef3960855e4e2b56138
Beren Millidge <bmillidgework@gmail.com>
2018-08-14 17:45:36 +0100

    Add files via upload

---
+++ src/test_example.rs
@@ -0,0 +1,25 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+}
+
+
+#[derive(Debug)]
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
